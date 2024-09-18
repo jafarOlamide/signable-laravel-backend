6def1bbf6d5c7028f6ad82fa838b84516f4df305
jafarOlamide <jafarolamidekale@gmail.com>
2024-09-18 15:00:34 +0100

    tests refactor

--- tests/Unit/GeneralChatMessageEventTest.php
+++ tests/Unit/GeneralChatMessageEventTest.php
@@ -6,24 +6,32 @@ use App\Events\GeneralChatMessageSent;
 use App\Models\Message;
 use App\Models\User;
 use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Support\Facades\Event;
 use PHPUnit\Framework\TestCase;
 
 class GeneralChatMessageEventTest extends TestCase
 {
+
+    private $message;
+
+    private GeneralChatMessageSent $event;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->message = new Message();
+        $this->event = new GeneralChatMessageSent($this->message);
+    }
+
     public function test_constructor_sets_message_model_as_property()
     {
-        $message = new Message();
-        $event = new GeneralChatMessageSent($message);
-
-        $this->assertSame($message, $event->message);
+        $this->assertSame($this->message, $this->event->message);
     }
 
     public function test_broadcast_on_returns_correct_channel_route()
     {
-        $message = new Message();
-        $event = new GeneralChatMessageSent($message);
-
-        $channels = $event->broadcastOn();
+        $channels = $this->event->broadcastOn();
 
         $this->assertIsArray($channels);
         $this->assertCount(1, $channels);
